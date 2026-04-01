5c337a90aee88d67232a3cf9564db7212c0f1a5c
Seldom <149964604+teldommm@users.noreply.github.com>
2026-04-01 22:53:23 +0300

    Update ntsync.h

--- .github/actions/ntsync/ntsync.h
+++ .github/actions/ntsync/ntsync.h
@@ -1,11 +1,8 @@
-
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
- * Kernel support for NT synchronization primitives
+ * Kernel support for NT synchronization primitive emulation
  *
- * Copyright (C) 2021-2024 Elizabeth Figura <zfigura@codeweavers.com>
- *
- * Backported header for Linux 6.6 (GKI Android).
+ * Copyright (C) 2021-2022 Elizabeth Figura <zfigura@codeweavers.com>
  */
 
 #ifndef __LINUX_NTSYNC_H
@@ -34,32 +31,29 @@ struct ntsync_wait_args {
 	__u64 timeout;
 	__u64 objs;
 	__u32 count;
-	__u32 owner;
 	__u32 index;
 	__u32 flags;
-	__u32 alert;    /* fd of alert event (0 = none) */
+	__u32 owner;
+	__u32 alert;
 	__u32 pad;
 };
 
 #define NTSYNC_MAX_WAIT_COUNT 64
 
-/* Device ioctls */
-#define NTSYNC_IOC_CREATE_SEM		_IOWR('N', 0x80, struct ntsync_sem_args)
-#define NTSYNC_IOC_CREATE_MUTEX		_IOWR('N', 0x81, struct ntsync_mutex_args)
-#define NTSYNC_IOC_CREATE_EVENT		_IOWR('N', 0x82, struct ntsync_event_args)
-#define NTSYNC_IOC_WAIT_ANY		_IOWR('N', 0x83, struct ntsync_wait_args)
-#define NTSYNC_IOC_WAIT_ALL		_IOWR('N', 0x84, struct ntsync_wait_args)
+#define NTSYNC_IOC_CREATE_SEM		_IOW ('N', 0x80, struct ntsync_sem_args)
+#define NTSYNC_IOC_WAIT_ANY		_IOWR('N', 0x82, struct ntsync_wait_args)
+#define NTSYNC_IOC_WAIT_ALL		_IOWR('N', 0x83, struct ntsync_wait_args)
+#define NTSYNC_IOC_CREATE_MUTEX		_IOW ('N', 0x84, struct ntsync_mutex_args)
+#define NTSYNC_IOC_CREATE_EVENT		_IOW ('N', 0x87, struct ntsync_event_args)
 
-/* Object ioctls — names as used in ntsync.c */
-#define NTSYNC_IOC_SEM_RELEASE		_IOWR('N', 0x88, __u32)
-#define NTSYNC_IOC_SEM_READ		_IOR ('N', 0x89, struct ntsync_sem_args)
-#define NTSYNC_IOC_MUTEX_UNLOCK		_IOWR('N', 0x8a, struct ntsync_mutex_args)
-#define NTSYNC_IOC_MUTEX_KILL		_IOW ('N', 0x8b, __u32)
+#define NTSYNC_IOC_SEM_RELEASE		_IOWR('N', 0x81, __u32)
+#define NTSYNC_IOC_MUTEX_UNLOCK		_IOWR('N', 0x85, struct ntsync_mutex_args)
+#define NTSYNC_IOC_MUTEX_KILL		_IOW ('N', 0x86, __u32)
+#define NTSYNC_IOC_EVENT_SET		_IOR ('N', 0x88, __u32)
+#define NTSYNC_IOC_EVENT_RESET		_IOR ('N', 0x89, __u32)
+#define NTSYNC_IOC_EVENT_PULSE		_IOR ('N', 0x8a, __u32)
+#define NTSYNC_IOC_SEM_READ		_IOR ('N', 0x8b, struct ntsync_sem_args)
 #define NTSYNC_IOC_MUTEX_READ		_IOR ('N', 0x8c, struct ntsync_mutex_args)
-#define NTSYNC_IOC_EVENT_SET		_IOR ('N', 0x8d, struct ntsync_event_args)
-#define NTSYNC_IOC_EVENT_RESET		_IOR ('N', 0x8e, struct ntsync_event_args)
-#define NTSYNC_IOC_EVENT_PULSE		_IOR ('N', 0x8f, struct ntsync_event_args)
-#define NTSYNC_IOC_EVENT_READ		_IOR ('N', 0x90, struct ntsync_event_args)
-#define NTSYNC_IOC_KILL_OWNER		_IOW ('N', 0x91, __u32)
+#define NTSYNC_IOC_EVENT_READ		_IOR ('N', 0x8d, struct ntsync_event_args)
 
-#endif /* __LINUX_NTSYNC_H */
+#endif
